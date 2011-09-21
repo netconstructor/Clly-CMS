7601fa96fb23b0283ffb2d9c83c1e36298e0af5b
Joel Niedfeldt <joel@niedfeldt.com>
2011-09-21 15:10:02 -0400

    changes for email contents

--- app/controllers/cllycms/content_controller.rb
+++ app/controllers/cllycms/content_controller.rb
@@ -1,6 +1,27 @@
 module Cllycms
   class ContentController < ApplicationController
+    require 'mail'
+    require 'mongoid'
+    skip_before_filter :verify_authenticity_token
+   
     def index
+   
+    end
+    
+    def email
+#       message = Mail.new(params[:message])
+#       Rails.logger.log message.subject #print the subject to the logs
+#       Rails.logger.log message.body.decoded #print the decoded body to the logs
+#       Rails.logger.log message.attachments.first.inspect #inspect the first attachment
+#   
+#       # Do some other stuff with the mail message
+
+      c=Cllycms::Content.new       
+      c.title=params[:subject]
+      c.content_notes.build(:entry=>params[:plain])
+      c.save 
+  
+      render :text => 'success', :status => 200 # a status of 404 would reject the mail      
     end
   
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Cllycms::Engine.routes.draw do
   root :to=>"content#index"
   #get "content/index"
+  
+  match "email" => "content#email"
 
 end
